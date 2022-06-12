05b50f87ec9f7d46810a27966e84143dac3489a5
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-13 01:50:57 +0530

    Create BuzzNumber.java

---
+++ BuzzNumber.java
@@ -0,0 +1,12 @@
+class Buzz{
+
+    public static void main(String[] args) {
+        int n = 631;
+        if(n%10==7 || n%7 ==0)
+        {
+            System.out.println("buzz number");
+        }
+        else
+            System.out.println("not");
+    }
+}
