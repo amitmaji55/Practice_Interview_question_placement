63b9c841c1f4f15e57e9733b2e772c81e9f3b3d9
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-13 01:55:13 +0530

    Create Perfect_Number.java

---
+++ Perfect_Number.java
@@ -0,0 +1,20 @@
+class perfect_number{
+
+    public static void main(String[] args) {
+        int n = 1;
+        int sum = 0;
+         for (int i=1;i<n;i++)
+         {
+             if(n%i==0)
+             {
+                 sum = sum + i;
+             }
+         }
+         if(sum==n)
+         {
+             System.out.println("perfect number");
+         }
+         else
+             System.out.println("Not a perfect number");
+    }
+}
