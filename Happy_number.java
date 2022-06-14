c9ca1aef23989d3a27bc07efa70b51ff6d4fcca7
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-14 23:13:38 +0530

    Update Happy_number.java

--- Happy_number.java
+++ Happy_number.java
@@ -1,29 +1,24 @@
-class Happy_Number{
-    public static int calculate(int n)
-    {
-        int sum = 0;
-        while (n!=0)
-        {
-            int rem = n%10;
-            sum = sum + (rem*rem);
-            n = n/10;
-        }
-        return sum;
-    }
-    public static  boolean is_happy_number(int n)
-    {
-        int temp = n;
-        while(temp>9)
-        {
-            temp=calculate(n);
-        }
-        if(temp==1)
-            return true;
-        return false;
-    }
 
+public class Happy_Number{
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println(is_happy_number(n));
+        int sum = 0, rem;
+        int n = 320;
+        while(n>9)
+        {
+            sum  = 0;
+            while(n!=0)
+            {
+                rem = n%10;
+                sum = sum+ (rem*rem);
+                n = n/10;
+            }
+            n = sum;
+        }
+        if(sum==1)
+        {
+            System.out.println("Happy Number");
+        }
+        else
+            System.out.println("Not a happy Number");
     }
 }
