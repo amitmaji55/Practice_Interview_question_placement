e3586386e8dd37132d3de2e53eb450f29f81fbef
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-14 23:06:25 +0530

    Update Magic_Number.java

--- Magic_Number.java
+++ Magic_Number.java
@@ -1,18 +1,21 @@
-class Magic_number{
+class Magic_Number{
+
     public static void main(String[] args) {
-        int n = 20;
-        int sum = 0, rem ;
-        while(n>0 || sum>9)
-        {
-            rem = n%10;
-            sum = sum+rem;
-            n = n/10;
+        int n = 199999;
+        int rem;
+        int sum = 0;
+        while (n > 9) {
+            sum = 0;
+            while (n != 0) {
+                rem = n % 10;
+                n = n / 10;
+                sum = sum + rem;
+            }
+            n = sum;
         }
-        if(sum==1)
-        {
-            System.out.println("Happy Number");
-        }
+        if (sum == 1)
+            System.out.println("Magic Number");
         else
-            System.out.println("Not a happy number");
+            System.out.println("Not Magic Number");
     }
 }
