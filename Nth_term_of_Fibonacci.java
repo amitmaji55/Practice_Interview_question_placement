1dcdc928dc9df77c92471914c53703ee32d087b8
AMIT MAJI <82029428+amitmaji55@users.noreply.github.com>
2022-06-15 23:35:33 +0530

    Create Nth_term_of_Fibonacci.java

---
+++ Nth_term_of_Fibonacci.java
@@ -0,0 +1,15 @@
+class  Nth_term_of_Fibonacci
+{
+    public static int fibo(int n)
+    {
+        if(n<=1)
+            return n;
+        return fibo(n-1) + fibo(n-2);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fibo(n));
+    }
+}
